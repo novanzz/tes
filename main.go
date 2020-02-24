ad56ad87b519106f6be206ab4c56c795d6f794b7
novanramadhan <novanramadhanhidayat@gmail.com>
2020-02-24 14:32:02 +0700

    revert

--- main.go
+++ main.go
@@ -8,6 +8,9 @@ func main() {
     fmt.Println("branch baru ahaide")
     fmt.Println("hello world")
     fmt.Println("jangkrik")
+<<<<<<< HEAD
     fmt.Println("test revert")
     fmt.Println("test revert 2")
+=======
+>>>>>>> parent of 1804c8e... revert
 }
